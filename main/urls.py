259ade72f12193634822f2fa69bf1b501589cb75
stealstick <stillstick40@gmail.com>
2018-04-02 15:27:12 +0900

    개인정보 처리방침

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^policy$', views.policy),
+    url(r'^term$', views.term)
+    
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/main.html',)
+    
+
+def policy(request):
+    return render(request, 'rules/privacy.html',)
+
+
+def term(request):
+    return render(request, 'rules/term-service.html',)
